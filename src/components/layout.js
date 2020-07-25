3397927d2fd24ff3db9c8d42f30fea4d05c85e65
Aarón <aaron.rc24@gmail.com>
2020-07-25 02:58:43 -0500

    Agregado en GraphQL el menú principal ordenado. \n Se quitó Header \n Ajustes de tamaño.

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
+import Menu from "./menu"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -20,21 +21,34 @@ const Layout = ({ children }) => {
           title
         }
       }
+      allWordpressPage {
+        edges {
+          node {
+            id
+            title
+            slug
+            menu_order
+          }
+        }
+      }
     }
   `)
 
+
+  const menu = data.allWordpressPage.edges
+  menu.sort((a,b) => (a.node.menu_order > b.node.menu_order) ? 1 : ((b.node.menu_order < a.node.menu_order) ? -1 : 0)); 
   return (
     <>
-      <Header style={{ textAlign: 'center'}} siteTitle={data.site.siteMetadata.title} />
+      {/*<Header style={{ textAlign: 'center'}} siteTitle={data.site.siteMetadata.title} />*/}
+      <Menu menu={menu} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: "60%",
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer></footer>
       </div>
     </>
   )
