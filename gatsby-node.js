1447a178b8d12b75ac85211de6c309982b56f3cc
Aarón <aaron.rc24@gmail.com>
2020-07-25 02:54:55 -0500

    Implementado createPages() y createPage() para construcción dinámica de páginas.

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,40 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node }) => {
+  // console.log(node)
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            slug
+            title
+            content
+          }
+        }
+      }
+    }
+  `)
+  result.data.allWordpressPage.edges.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve(`./src/pages/page.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        content: node.content,
+        slug: node.slug
+      },
+    })
+  })
+}
+
+
