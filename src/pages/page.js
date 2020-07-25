179f718ba31a822f1fc39634cb1eb5b0cc2ef659
Aarón <aaron.rc24@gmail.com>
2020-07-25 02:52:39 -0500

    Plantilla para mostrar todas las páginas. El contenido se filtra mediante GraphQL.

---
+++ src/pages/page.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Footer from "../components/footer"
+
+export default function Page({ data }){
+  const page = data.allWordpressPage.edges[0].node
+  const lastUpdated = <p className="p-last-updated"> Last Updated: {page.modified} UTC</p>
+
+  return (<Layout>
+    <SEO title={page.title} />
+    <h2 className="text-center"> {page.title} </h2>
+    <div dangerouslySetInnerHTML={{__html: page.content}} />
+
+    <Footer rightContent={ lastUpdated } />
+  </Layout>)
+}
+
+export const pageQuery = graphql` 
+query ($slug: String!) {
+  allWordpressPage(filter: {slug: {eq: $slug}}) {
+    edges {
+      node {
+        title
+        content
+        slug
+        date(formatString: "YYYY-MM-DD")
+        modified(formatString: "YYYY-MM-DD HH:mm:ss")
+      }
+    }
+  }
+}
+`
