cc34b0bc60c0acdcc7fca58b89751387a6a57776
Aarón <aaron.rc24@gmail.com>
2020-08-06 16:48:18 -0500

    Se elimina el atributo href='' del ícono del menú

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby";
 
-export default function Menu({ menu }) {
+export default function Menu({ menu, siteTitle }) {
   const toggleMenu = () => {
     var x = document.getElementById("mainMenu");
     if (x.className === "topnav") {
@@ -14,11 +14,12 @@ export default function Menu({ menu }) {
   const links = menu.map((item, index) => {
     const link = item.node
     const to = "/" + link.slug
-    return <Link key={index} to={to} activeClassName="active">{ link.title }</Link>
+    return <Link key={index} to={to} className="menu-item" activeClassName="active">{ link.title }</Link>
   })
 
   return(
     <div className="topnav" id="mainMenu">
+      <Link to="/" className="home-link">{siteTitle}</Link>
       <a className="icon" onClick={() => { toggleMenu(); }}>
         <i className="burger-menu">&equiv;</i>
       </a>
