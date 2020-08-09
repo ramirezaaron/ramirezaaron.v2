c90406226b3e4982ace60bcf4f47ced4d4e2d7d8
Aarón <aaron.rc24@gmail.com>
2020-08-09 00:56:36 -0500

    Validación del elemento data.allWordpressPage.edges[0]

--- src/pages/page.js
+++ src/pages/page.js
@@ -3,22 +3,23 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Footer from "../components/footer"
+//import Footer from "../components/footer"
 
 export default function Page({ data }){
+  if(data.allWordpressPage.edges[0] == undefined)
+    return <></>
+
   const page = data.allWordpressPage.edges[0].node
-  const lastUpdated = <p className="p-last-updated"> Last Updated: {page.modified} UTC</p>
+  //const lastUpdated = <p className="p-last-updated"> Last Updated: {page.modified} UTC</p>
 
   return (<Layout>
     <SEO title={page.title} />
     <h2 className="text-center"> {page.title} </h2>
     <div dangerouslySetInnerHTML={{__html: page.content}} />
-
   </Layout>)
 }
 //<Footer rightContent={ lastUpdated } />
 
-// Se asigna $slug = "hello" porque es la página inicial 
 export const pageQuery = graphql` 
 query ($slug: String = "hello") {
   allWordpressPage(filter: {slug: {eq: $slug}}) {
