272fb6580ebf3aae95442b273f8661861a2f4dfc
Aarón <aaron.rc24@gmail.com>
2020-08-09 00:39:35 -0500

    Cambio del comportamiento del menú responsivo. Ajuste para procesar elementos del menú obtenido por API REST

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,24 +4,26 @@ import { Link } from "gatsby";
 export default function Menu({ menu, siteTitle }) {
   const toggleMenu = () => {
     var x = document.getElementById("mainMenu");
+    var spanMenu = document.getElementById("menu-trigger")
     if (x.className === "topnav") {
       x.className += " responsive";
+      spanMenu.innerHTML = "&times;"
     } else {
       x.className = "topnav";
+      spanMenu.innerHTML = "&bull;&bull;&bull;"
     }
   }
   
   const links = menu.map((item, index) => {
-    const link = item.node
-    const to = "/" + link.slug
-    return <Link key={index} to={to} className="menu-item" activeClassName="active">{ link.title }</Link>
+    const to = "/" + item.object_slug
+    return <Link key={index} to={to} className="menu-item" activeClassName="active">{ item.title }</Link>
   })
 
   return(
     <div className="topnav" id="mainMenu">
       <Link to="/" className="home-link">{siteTitle}</Link>
       <a className="icon" onClick={() => { toggleMenu(); }}>
-        <i className="burger-menu">&equiv;</i>
+        <span id="menu-trigger" className="burger-menu">&bull;&bull;&bull;</span>
       </a>
       {links}
   </div>)  
